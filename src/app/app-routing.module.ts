3f9c17ef5205f3866e272b4cb911ac1021d255de
nvphuc <Phuc.NguyenVan@nttdata.com>
2017-11-29 16:59:01 +0700

    add signup form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {JokeComponent} from './joke/joke.component';
 import {JokeListComponent} from './joke-list/joke-list.component';
+import {SignupFormComponent} from './signup-form/signup-form.component'
 
 const routes: Routes = [
-    {path:'', redirectTo: '/jokes',pathMatch:'full'},
+    {path:'', redirectTo: '/signup',pathMatch:'full'},
     {path:'joke', component: JokeComponent},
-    {path:'jokes', component: JokeListComponent}
+    {path:'jokes', component: JokeListComponent},
+    {path:'signup', component: SignupFormComponent}
 ];
 @NgModule({
 imports: [ RouterModule.forRoot(routes) ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { JokeComponent } from './joke/joke.component';
 import { AppRoutingModule} from './app-routing.module';
 import { JokeListComponent } from './joke-list/joke-list.component';
 import { JokeFormComponent } from './joke-form/joke-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JokeComponent,
     JokeListComponent,
-    JokeFormComponent
+    JokeFormComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
